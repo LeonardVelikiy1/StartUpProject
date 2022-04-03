18ab5aeb286fefd9656af305c25c952ce3a8c1d5
LeonardVelikiy1 <nickgolovash@gmail.com>
2022-04-03 04:27:40 +0400

    Сделал перемещение и смену дня и ночи

--- Assets/Scripts/Run_Test.cs
+++ Assets/Scripts/Run_Test.cs
@@ -5,26 +5,39 @@ using UnityEngine.UI;
 
 public class Run_Test : MonoBehaviour
 {
-    // Start is called before the first frame update
-
+   
+    public float speed = 4f;
     private Light myLight;//приватная переменная не отобразится в юнити 
     public GameObject spher;//сюда можно притащить объект и обращаться к нему
     void Start()
     {
-
         myLight = GetComponent <Light>(); //myLight<--переменная = GetComponent<-- функция для выбора Компонента, сам Компонент--> <Light>();
-
     }
-    // Update is called once per frame
+  
     void Update()
     {
+       //выключение света 
        if (Input.GetKey(KeyCode.Space))//проверка нажатия на пробел 
         {
-            myLight.enabled = !myLight.enabled;//enabled(это галочка на Компоненте ну либо просто включение выключение Компонента в данном случае это свет)
+            myLight.transform.Rotate (Vector3.left * speed * Time.deltaTime);
+            
         }
+
+       //изсенение цвета объекта по кнопке 
        if (Input.GetKey(KeyCode.F))
         {
             spher.GetComponent <Renderer>().material.color =Color.black;//изменение цвета перерендер объекта полагаю
+            
         }
+        //перемещение
+        if (Input.GetKey(KeyCode.W))
+            spher.transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.S))
+            spher.transform.Translate(-Vector3.forward * speed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.A))
+            spher.transform.Translate(Vector3.left * speed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.D))
+            spher.transform.Translate(Vector3.right * speed * Time.deltaTime);
+        
     }
 }
