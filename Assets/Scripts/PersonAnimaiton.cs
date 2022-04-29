25708d7778e6298106195ed13be51f28e598b88f
LeonardVelikiy1 <nickgolovash@gmail.com>
2022-04-30 01:02:22 +0400

    пока работает нормально не трогайте анимацию

---
+++ Assets/Scripts/PersonAnimaiton.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonAnimaiton : MonoBehaviour
+{
+    private Animator _animator;
+    
+
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+        {
+            print("движение");
+            _animator.SetBool("RunActiv", true);
+        }
+        else 
+        {
+            _animator.SetBool("RunActiv", false);
+        }
+    }
+}
